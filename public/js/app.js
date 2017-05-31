2409df91e9c22ed66d9b945ddfb2d3bd40724014
romamaxime <roma.maxime1994@gmail.com>
2017-05-31 15:31:53 +0200

    ajout de style dans infos-pratiques

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,11 @@
 $(".button-collapse").sideNav();
 
 $('.parallax').parallax();
+
+ var options = [
+    {selector: '#list-program', offset: 400, callback:function(el) {
+      Materialize.fadeInImage($(el));
+      Materialize.showStaggeredList($(el));;;
+    } },
+  ];
+  Materialize.scrollFire(options);
